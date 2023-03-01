144f3e0673eb32ac9118708775e17b94e6b57bce
Amrit Poudel <aamreet12345@gmail.com>
2023-03-01 21:23:43 +0545

    background noise remover(tried but failed no audiable results)

---
+++ backgroundSoundRemove.py
@@ -0,0 +1,56 @@
+import os
+import glob
+import sounddevice as sd
+import soundfile as sf
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+# Set the duration and sample rate of the recording
+duration = 10  # in seconds
+sample_rate = 44100  # in Hz
+
+# Define the silence threshold (in dB)
+silence_threshold = -45
+
+# Define a function to clean an audio file
+def clean_audio(file_path):
+    # Load the audio file
+    audio_file = AudioSegment.from_file(file_path, format=file_path.split(".")[-1])
+
+    # Split the audio on silence
+    chunks = split_on_silence(audio_file, 
+                              min_silence_len=500, 
+                              silence_thresh=silence_threshold)
+
+    # Combine the non-silent chunks
+    clean_audio = chunks[0]
+    for i in range(1, len(chunks)):
+        clean_audio += chunks[i]
+
+    # Save the clean audio
+    clean_file_path = file_path.split(".")[0] + "_clean.wav"
+    clean_audio.export(clean_file_path, format="wav")
+    print(f"Clean audio saved to {clean_file_path}")
+
+# Record audio from the user
+print("Recording...")
+audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+sd.wait()
+print("Recording finished!")
+
+# Save the audio to a WAV file
+file_name = "user_audio.wav"
+sf.write(file_name, audio, sample_rate)
+print(f"Audio saved to {file_name}")
+
+# Clean the recorded audio
+print("Cleaning recorded audio...")
+clean_audio(file_name)
+
+# Define the directory containing the audio files
+audio_dir = "/path/to/audio/directory"
+
+# Clean all the audio files in the directory
+for file_path in glob.glob(os.path.join(audio_dir, "*.wav")):
+    print(f"Cleaning {file_path}...")
+    clean_audio(file_path)
